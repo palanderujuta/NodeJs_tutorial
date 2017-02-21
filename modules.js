bc28ee20ecec3cceadf3570ac63132b5f4de9bbe
palanderujuta <palanderujuta@gmai.com>
2017-02-21 15:05:14 -0500

    modules example

---
+++ modules.js
@@ -0,0 +1,28 @@
+///OS module
+
+var os = require("os");
+// Endianness
+console.log('endianness : ' + os.endianness());
+
+// OS type
+console.log('type : ' + os.type());
+// OS platform
+console.log('platform : ' + os.platform());
+// Total system memory
+console.log('total memory : ' + os.totalmem() + " bytes.");
+// Total free memory
+console.log('free memory : ' + os.freemem() + " bytes.");
+
+//path module
+
+var path = require("path");
+// Normalization
+console.log('normalization : ' +
+path.normalize('/test/test1//2slashes/1slash/tab/..'));
+// Join
+console.log('joint path : ' + path.join('/test', 'test1', '2slashes/1slash',
+'tab', '..'));
+// Resolve
+console.log('resolve : ' + path.resolve('main.js'));
+// extName
+console.log('ext name : ' + path.extname('main.js'));
